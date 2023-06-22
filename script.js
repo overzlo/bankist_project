6433874a90d2e30725ad30501cc79aa3dbd63bfc
overzlo <flashzed_969@mail.ru>
2023-06-22 09:39:19 +0600

    raw

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ info.innerHTML = ''
 let balance = $(".balance");
 const logForm = $(".login")
 const system = $(".system")
+
+let arrS = [];
+let arrO = [];
 function enter(){
     const logForm = $(".login")
 
@@ -25,13 +28,14 @@ function enter(){
 
 }
 
+
 function userName(name, password){
     const userinfo = $(".username");
-    userinfo.innerHTML = `${name} ${password}`
+    userinfo.innerHTML = `<h3>${name} ${password}</h3>`
     let balance = $(".balance");
     for(let i = 0; i < users.length;i++){
         if(name == users[i].name && password == users[i].pin ){
-            balance.innerHTML = `Current balance: ${users[i].balance}`;
+            balance.innerHTML = `<h3>Current balance: ${users[i].balance}</h3>`;
         }   
 
 }
@@ -42,24 +46,25 @@ function transfer(){
     const transferUserAmount = Number(transferUser[1].value);
 
     for(let i = 0; i < users.length; i ++ ){
-        if(transferUserName == users[i].name && login[0] != transferUserName){
+        if(transferUserName == users[i].name && login[0].value!= transferUserName){
             users[i].balance += transferUserAmount;
-            
+        
             info.insertAdjacentHTML("beforeend", `<h4>Transfer to ${transferUserName}.  Amount of transfer: ${transferUserAmount}</h4>`)
-        }
-
-        for(let i = 0; i < users.length; i++){
-            if(users[i].name == login[0].value && users[i].pin == login[1].value){
-                users[i].balance -= transferUserAmount;
-                balance.innerHTML = `Current balance: ${users[i].balance}`;
-
+            arrO.push(`<h4>Transfer to ${transferUserName}.  Amount of transfer: ${transferUserAmount}</h4>`)
+            for(let j = 0; j < users.length;j++){
+                if(users[j].name == login[0].value && users[j].pin == login[1].value){
+                    users[j].balance -= transferUserAmount;
+                    balance.innerHTML = `<h3>Current balance: ${users[i].balance}</h3>`;
+                }
             }
         }
+
+      
     }   
 
     localStorage.setItem('userss', JSON.stringify(users));
 }
-
+console.log(arrS);
 function loan(){
     const loan = document.getElementsByName("loan")
     const loanAmount = Number(loan[0].value);
@@ -69,7 +74,8 @@ function loan(){
             users[i].balance +=loanAmount;
             console.log(users[i].balance);
             info.insertAdjacentHTML("beforeend", `<h4>Loan ${Number(loan[0].value)}</h4>`)
-            balance.innerHTML = `Current balance: ${users[i].balance}`;
+            arrS.push(`<h4>Loan ${Number(loan[0].value)}</h4>`)
+            balance.innerHTML = `<h3>Current balance: ${users[i].balance}</h3>`;
 
         }
     }
@@ -95,4 +101,27 @@ function logOUT(){
     const system = $(".system")
     system.classList.add('hidden')
     logForm.classList.remove('hidden')
+}
+
+
+function clearAll(){
+    info.innerHTML = '';
+    console.log("Clear")
+}
+function sortByIncome(){
+
+    info.innerHTML = '';
+    for (let i = 0; i < arrS.length; i++) {
+  
+        info.insertAdjacentHTML("beforeend", `<h4 class="income">${arrS[i]}</h4>`);
+      
+    }
+}
+function sortByOutcome(){
+    info.innerHTML = '';
+    for (let i = 0; i < arrS.length; i++) {
+  
+        info.insertAdjacentHTML("beforeend", `<h4 class="income">${arrO[i]}</h4>`);
+      
+    }
 }
